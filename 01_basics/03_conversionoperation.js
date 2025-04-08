e937bc264b6ed6e0062571b26d8216efd8641048
shrutirawani <shrutirawaniii@gmail.com>
2025-04-08 16:08:25 +0000

    conversiondata

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,41 @@
+let score = "55abx"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+// let's suppose that the 55 is a string value and we want to convert it to number// 
+
+let valueinNumber = Number(score)
+//console.log(typeof valueinNumber)
+//console.log(valueinNumber)
+
+//to convert the value to number => Number(var) // 
+
+// 33 = 33
+//"33" = 33
+//"33abx" = NaN
+//true = 1; false = 0
+//null = 0
+//undefined = NaN
+//"" = 0 //
+
+// to convert the number to boolean 
+
+let isLoggedin = 1 
+
+let booleanisLoggedin = Boolean(isLoggedin)
+console.log(booleanisLoggedin)
+ 
+// 1=true, 0= false
+// "" = false
+//"shruti" = true 
+
+let age = 21
+
+let stringNumber = String(age)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+//to convert the value to string = String(var)
+
+
