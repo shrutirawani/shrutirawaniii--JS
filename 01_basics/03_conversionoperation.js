7fd10f2bbbc9fd87a960d621b78fd9d6b937f02c
shrutirawani <shrutirawaniii@gmail.com>
2025-04-09 15:26:05 +0000

    updated operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -6,8 +6,8 @@ console.log(typeof (score))
 // let's suppose that the 55 is a string value and we want to convert it to number// 
 
 let valueinNumber = Number(score)
-//console.log(typeof valueinNumber)
-//console.log(valueinNumber)
+console.log(typeof valueinNumber)
+console.log(valueinNumber)
 
 //to convert the value to number => Number(var) // 
 
@@ -39,3 +39,50 @@ console.log(typeof stringNumber)
 //to convert the value to string = String(var)
 
 
+// operations.....................// 
+
+let value = 3
+let negvalue = -value 
+console.log(negvalue)
+
+// console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2%2)
+//console.log(2**3) 2 power 3
+//console.log(2/2)
+
+let string1= ("hello")
+let string2 = (" Shruti")
+let string3 = string1 + string2
+
+console.log(string3)
+
+// console.log(1+"2")
+// console.log("1"+ 2)
+// console.log("1"+2+2) // if string is first then every value is treated as string
+// console.log(1+2+"2") // if string is at the last then it will be treated as a string and for other operation will be performed
+// console.log(1+2+"3"+3)
+// console.log(1+2+"3"+4+5) // wherever string comes the value after is treated as string and before can the operation be done
+
+
+console.log(+true)
+console.log(+"")
+
+let gamecounter = 100
+++gamecounter          // gamecounter++ = 101
+console.log(gamecounter)
+
+// postfix = x++ it first assigns x to y (which is 5), then increments x to 6.
+
+let x = 5
+let y = x++ 
+console.log(y) // 5
+console.log(x) // 6
+
+//prefix - It first increments x to 6, then assigns that value to y.
+
+let x = 5
+let y = ++x
+console.log(x)
+console.log(y)
